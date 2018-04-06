446832bab01b5c4b9303fa21879b4aa7352f09b5
Adam Kloboucnik <adam@klob.cz>
2018-04-06 12:18:49 +0200

    Enhance block frontend application to show last 20 blocks rovered

--- assets/apps/blocks/src/app.jsx
+++ assets/apps/blocks/src/app.jsx
@@ -4,26 +4,57 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow-disabled
+ * @flow
  */
-
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { concat, take } from 'ramda'
 
-const Io = require('socket.io-client')
+import Block from './Block.jsx'
 
-export default class App extends React.Component {
-  constructor() {
+type State = {
+  blocks: Object[]
+}
+export default class App extends Component<*, State> {
+  _socket: any
+
+  constructor () {
     super()
-
-    this._socket = Io({
-      path: '/ws'
-    })
+    this.state = { blocks: [] }
+    this._socket = new WebSocket(`ws://${location.hostname}:${location.port}/ws`) // eslint-disable-line
   }
 
-  render() {
-    return <h1>Hello</h1>
+  componentDidMount () {
+    this._socket.onmessage = (data) => {
+      this.setState({ blocks: concat(this.state.blocks, [JSON.parse(data.data)]) })
+    }
+  }
+
+  componentWillUnmount () {
+    this._socket.close()
+  }
+
+  render () {
+    const blocks = take(20, this.state.blocks).sort((a, b) => b.timestamp - a.timestamp).map(block => {
+      return <Block {...block} key={block.data.hash} />
+    })
+    return (
+      <div className='container'>
+        <h1>Blockcollider</h1>
+        <div className='container'>
+          <h2>Collected blocks (last 20)</h2>
+          <div className='d-flex flex-wrap flex-row'>
+            {blocks}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const appEl = document.getElementById('app')
+if (appEl) {
+  ReactDOM.render(<App />, appEl)
+} else {
+  console.error('Cannot find element with id "app"')
+}
