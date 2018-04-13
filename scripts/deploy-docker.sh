277f20575f51fd99db078092c457f90c12b8707a
Tomas Korcak <korczis@gmail.com>
2018-04-14 01:51:37 +0200

    Add some secret env variables

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# See https://docs.travis-ci.com/user/build-stages/share-docker-image/
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker build -t blockcollider/bcnode:${TRAVIS_BRANCH} ..
+    docker push blockcollider/bcnode:${TRAVIS_BRANCH}
 fi
