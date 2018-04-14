9b5bf30494186358c31f9e79272361edd3b24027
Tomas Korcak <korczis@gmail.com>
2018-04-14 02:01:39 +0200

    Explicit tagging of docker builds created by CI

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -3,6 +3,7 @@
 # See https://docs.travis-ci.com/user/build-stages/share-docker-image/
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build -t blockcollider/bcnode:${TRAVIS_BRANCH} ..
+    docker build -t blockcollider/bcnode ..
+    docker tag blockcollider/bcnode blockcollider/bcnode:${TRAVIS_BRANCH}
     docker push blockcollider/bcnode:${TRAVIS_BRANCH}
 fi
