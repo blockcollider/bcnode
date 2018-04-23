98314c51b7281a678aca63c6effe365c496ae9ac
Tomas Korcak <korczis@gmail.com>
2018-04-23 15:02:07 +0200

    Dockerfiles for rendezvous-webrtc & rendezvous-websocket

---
+++ .docker/rendezvous-websocket/build.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+docker build -t blockcollider/rendezvous-websocket .
---
+++ .docker/rendezvous-websocket/run.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+docker run -d --name rendezvous-websocket -p 9090:9090 blockcollider/rendezvous-websocket
+
